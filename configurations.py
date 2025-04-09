8b6c6ca9b85d6fc0ce04386be2990189571bbbdd
nkumar20-wmp <nkumar20@woolworths.com.au>
2025-04-10 00:23:57 +1000

    Create configurations.py

---
+++ configurations.py
@@ -0,0 +1,21 @@
+#owner - nehal.kumar@mydeal.com.au
+
+'''
+@configurations file 
+
+'''
+# Set Google Cloud project ID
+PROJECT_ID = 'mydeal-bigquery'
+
+# Insert the dataset ID
+DATASET_ID = 'social'
+
+#Table ID
+TABLE_ID = 'facebook'
+
+# Facebook ads configuration 
+
+APP_ID = '615864062906887'
+APP_SECRET = 'ce85de03ff1863d6c21636dedec91574'
+ACCOUNT_ID = 'act_648849926128939'
+
